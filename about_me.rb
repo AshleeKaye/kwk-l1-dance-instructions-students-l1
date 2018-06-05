d40f76f6114810a20818cc39e64ce4a149acdf22
AshleeKaye <ashleemk@hotmail.com>
2018-06-05 16:34:06 +0000

    automatically backed up by learn

---
+++ about_me.rb
@@ -0,0 +1,18 @@
+def about_me
+  puts "My name is Ashlee"
+  puts "I grew up in Scottsdale, Paradise Valley, and Phoenix"
+  puts "I'm 14 years old"
+  puts "My favorite food is sushi"
+end
+
+10.times do
+about_me
+end
+
+#puts "How are you feeling?"
+#answer = gets.chomp
+#puts "So you're feeling #{answer}?"
+
+puts "How old are you?"
+answer = gets.chomp
+puts "So you're #{answer} years old."
--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,15 @@
 
 # Code your methods below
+def lower_body
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+  puts "Lift right foot"
+  puts "Retirn right foot to the ground"
+  puts "Finishing with a smal skip-step backward"
+end
+def skip_step_left
+  puts "Lower left foot to the ground"
+  puts "Bounce left foot back up slightly, kicking it a few inches back"
+  puts "Left, Right, Left and Left"
+end
+def skip_step_right
+  puts ""
